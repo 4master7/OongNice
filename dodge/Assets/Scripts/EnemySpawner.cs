20d2be64d1ab25cd9387b54144fd65d07616c342
4master7 <jtk040812@gmail.com>
2021-04-02 17:15:04 +0900

    so tired

---
+++ dodge/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemy;
+    public GameObject enemyGroup;
+    private bool isPlaying = true;
+
+    public void OffSpawner()
+    {
+        isPlaying = false;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isPlaying)
+        {
+            int spawnPer = Random.Range(0, 1001);
+            if (spawnPer < 1000)
+            {
+                Enemy e = Instantiate(enemy);
+
+                float posY = Random.Range(-4.3f, 4.3f);
+                int isLeftInstatiate = Random.Range(0, 2);
+
+                if (isLeftInstatiate == 0)
+                {
+                    e.transform.position = new Vector3(-8.3f, posY, -1);
+                    e.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    e.SetDirectionVector(1);
+                }
+                else
+                {
+                    e.transform.position = new Vector3(8.3f,posY,-1);
+                    e.transform.rotation = new Quaternion(0, 0, 180, 0);
+                    e.SetDirectionVector(-1);
+                }
+                e.transform.parent = enemyGroup.transform;
+            }
+        }
+    }
+}
---
+++ dodge/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int hp = 3;
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+    public void SetHP(int hp)
+    {
+        this.hp = hp;
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (hp > 0) 
+        {
+            float posX=0.0f;
+            float posY=0.0f;
+            posX = Input.GetAxisRaw("Horizontal");
+            posY = Input.GetAxisRaw("Vertical");
+            Vector3 pos = gameObject.transform.position;
+            pos.x += posX * Time.deltaTime * 10;
+            pos.y += posY * Time.deltaTime * 10;
+
+            if (pos.x < -8.3f)
+            {
+                pos.x = -8.3f;
+            }
+            else if (pos.x > 8.3f)
+            {
+                pos.x = 8.3f;
+            }
+
+            if (pos.y < -4.7f) 
+            {
+                pos.y = -4.7f;
+            }
+            else if (pos.y > 4.7f)
+            { 
+                pos.y = 4.7f;
+            }
+
+            gameObject.transform.position = pos;
+           }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.tag == "Enemy")
+        {
+            hp -= 1;
+            Debug.Log(hp);
+        }
+    }
+}
