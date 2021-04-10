179e114dddc15ea4169ab0578c9f06ea186f0a61
4master7 <jtk040812@gmail.com>
2021-04-10 16:50:59 +0900

    ff

---
+++ 3D rpg/Assets/rpg-tutorial-assets/Controller.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Interact focus;
+    public LayerMask movementMask;
+    Camera cam;
+    Motor motor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<Motor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+                RemoveFocus();
+            }
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                Interact interactable = hit.collider.gameObject.GetComponent<Interact>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+    void SetFocus(Interact newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+        newFocus.Onfocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+            focus = null;
+            motor.StopFollowing();
+        }
+    }
+
+}
---
+++ 3D rpg/Assets/rpg-tutorial-assets/PickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : Interact
+{
+    public RPGitem item;
+    public override void It()
+    {
+        base.It();
+        Pick();
+    }
+    void Pick()
+    {
+        Debug.Log("use Item : "+ item.name);
+        Destroy(gameObject);
+    }
+}
